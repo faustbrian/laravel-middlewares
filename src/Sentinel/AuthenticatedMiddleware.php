1bee7a5db9aaa756a3a4c4a12d3e808f7900ef22
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 12:49:29 +0200

    Applied fixes from StyleCI (#1)
    [ci skip] [skip ci]

--- src/Sentinel/AuthenticatedMiddleware.php
+++ src/Sentinel/AuthenticatedMiddleware.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Middlewares.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Middlewares\Sentinel;
 
 use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
