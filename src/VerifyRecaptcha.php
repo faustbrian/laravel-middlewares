d0247ce9ccdb7e7e1435e67c13f0bf6e92418189
Brian Faust <faustbrian@users.noreply.github.com>
2017-04-25 06:29:56 +0300

    Create VerifyRecaptcha.php

---
+++ src/VerifyRecaptcha.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Laravel Middlewares.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BrianFaust\Middlewares;
+
+use Closure;
+use Illuminate\Config\Repository;
+use Illuminate\Http\Request;
+use Vinkla\Recaptcha\Recaptcha;
+use Vinkla\Recaptcha\RecaptchaException;
+
+class VerifyRecaptcha
+{
+    private $config;
+
+    /**
+     * The middleware constructor.
+     *
+     * @param Repository $config
+     */
+    public function __construct(Repository $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $recaptcha = new Recaptcha($this->config->get('recaptcha.secret'));
+
+        try {
+            $recaptcha->verify($request->get('g-recaptcha-response') ?? 'invalid_captcha');
+        } catch (RecaptchaException $e) {
+            alert()->error(__('invalid_captcha'));
+
+            return back()->withInput();
+        }
+
+        return $next($request);
+    }
+}
