16c517da0fb338d70bfe8627b02d11952044037f
Brian Faust <hello@brianfaust.de>
2017-04-25 03:28:53 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/RoleMiddleware.php
+++ src/RoleMiddleware.php
@@ -11,11 +11,11 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
+
 namespace BrianFaust\Middlewares;
 
-use Closure;
 use Auth;
+use Closure;
 
 class RoleMiddleware
 {
